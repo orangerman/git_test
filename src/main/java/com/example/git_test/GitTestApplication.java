9616e2edeee22eededd0e78e1c5c480a44e14af1
Java_Fan <1057600474@qq.com>
2022-10-22 19:58:09 +0800

    uat 第一次提交

--- src/main/java/com/example/git_test/GitTestApplication.java
+++ src/main/java/com/example/git_test/GitTestApplication.java
@@ -3,6 +3,9 @@ package com.example.git_test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * uat 第一次提交
+ */
 @SpringBootApplication
 public class GitTestApplication {
 
